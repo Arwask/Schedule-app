5ffe98a1fa9f5bb8917cab53170c7b97384d8bdb
Arwa <arwa.it15@gmail.com>
2017-11-02 15:49:07 -0500

    edit employee details from manager view

--- controllers/employeeCtrl.js
+++ controllers/employeeCtrl.js
@@ -32,11 +32,52 @@ module.exports.getEmployeeDetails = (req, res, next) => {
     });
 };
 
+module.exports.editEmployeeDetailsForm = (req, res, next) => {
+  const { Employee, Department, sequelize } = req.app.get('models');
+  const data = {};
+  Department.findAll()
+    .then(Departments => {
+      data.depts = Departments;
+      Employee.findAll({
+        attributes: [[sequelize.fn('DISTINCT', sequelize.col('jobTitle')), 'jobTitle']]
+      }).then(titles => {
+        data.jobTitle = titles;
+        Employee.findOne({ include: [{ model: Department }], where: { id: req.params.employeeId } }).then(employee => {
+          data.employee = employee;
+          // res.json(data);
+          res.render('manager/employee-edit-form', { data });
+        });
+      });
+    })
+    .catch(err => {
+      next(err);
+    });
+};
+
 module.exports.editEmployeeDetails = (req, res, next) => {
-  const { Employee, Department } = req.app.get('models');
-  Employee.findOne({ include: [{ model: Department }], where: { id: req.params.employeeId } })
-    .then(employee => {
-      res.render('manager/employee-edit-form', { employee });
+  const { Employee } = req.app.get('models');
+  if (req.body.endDate === '') {
+    req.body.endDate = null;
+  }
+  let empObj = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    dob: req.body.dob,
+    phoneNumber: req.body.phoneNumber,
+    email: req.body.email,
+    departmentId: req.body.department,
+    jobTitle: req.body.jobTitle,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+    totalHours: req.body.totalHours,
+    notes: req.body.notes
+  };
+  Employee.update(empObj, {
+    where: { id: req.params.employeeId }
+  })
+    .then(() => {
+      // console.log(req.paarams.employeeId);
+      res.redirect(`/manager/manage-employee`);
     })
     .catch(err => {
       next(err);
